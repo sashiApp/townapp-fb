22a626ccef16459f63b30783443147c20f6ae570
sashis <sashis@sashis-MBP.fritz.box>
2018-05-28 05:15:53 +0530

    initial commit

---
+++ firebaseInit.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAyPvAeupZv-bcwpicK6co2z2rTPPXeZI0",
+    authDomain: "test-townapp.firebaseapp.com",
+    databaseURL: "https://test-townapp.firebaseio.com",
+    projectId: "test-townapp",
+    storageBucket: "",
+    messagingSenderId: "999014401754"
+  };
+  firebase.initializeApp(config);
